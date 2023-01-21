a28e65045adcc1d408f805fd4fa918f6264b73b4
Cesar Li (win10) <cesarli619@gmail.com>
2023-01-21 00:11:31 -0500

    Some review on polymorphism was done

--- designPatterns/src/ca/meself/object_oriented_programming/WorkingWithAnimals.java
+++ designPatterns/src/ca/meself/object_oriented_programming/WorkingWithAnimals.java
@@ -8,6 +8,29 @@ public class WorkingWithAnimals {
 		
 		clark.setName("Clarky");
 		System.out.println(clark.getName());
-		clark.digHole();	
+		clark.digHole();
+		
+		Animal doggy = new Dog();
+		Animal kitty = new Cat();
+		
+		System.out.println("Doggy says: " + doggy.getSound());
+		System.out.println("Kitty says: " + kitty.getSound());
+		
+		Animal[] animals = new Animal[2];
+		animals[0] = doggy;
+		animals[1] = kitty;		
+		
+		for (Animal animal : animals) {
+			System.out.println("Which animal sounds like this: " + animal.getSound());
+		}
+		
+		// Below will not work. Because is not defined in Animal, only Dog.
+		// doggy.digHole();
+		
+		((Dog) doggy).digHole();
+	}
+	
+	public static void changeObjectName(Dog dog) {
+		dog.setName("Bruce");
 	}
 }
