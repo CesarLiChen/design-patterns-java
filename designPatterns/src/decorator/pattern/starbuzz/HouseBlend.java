bc2f2c4ef4cb41bfae6fbd30cfcd080851292aa5
Cesar Li Chen <cesarli619@gmail.com>
2023-02-08 23:09:07 -0500

    Some clean up switch to if statements

--- designPatterns/src/decorator/pattern/starbuzz/HouseBlend.java
+++ designPatterns/src/decorator/pattern/starbuzz/HouseBlend.java
@@ -9,31 +9,17 @@ public class HouseBlend extends Beverage{
 	
 	@Override
 	public double cost() {
-//		return 0.89;
+		
 		double cost = 0.0;
 		Size size = this.getSize();
 		
-		switch (size) {
-		case TALL: 			
+		if (size == Size.TALL) {
 			cost = 0.89;
-			break;
-		case GRANDE: 
+		} else if (size == Size.GRANDE) {
 			cost = 0.90;
-			break;
-		case VENTI:
+		} else if (size == Size.VENTI) {
 			cost = 0.92;
-			break;
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + size);
 		}
-		
-//		if (getSize().equals(Size.TALL)) {
-//			cost = 0.89;
-//		} else if(getSize().equals(Size.GRANDE)) {
-//			cost = 0.90;
-//		} else {
-//			cost = 0.92;
-//		}
 		
 		return cost;
 	}
