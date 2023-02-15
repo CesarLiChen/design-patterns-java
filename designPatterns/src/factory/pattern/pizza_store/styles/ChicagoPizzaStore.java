d4067c6ac9bf9ff05acde526e2009d93b1af83f9
Cesar Li Chen <cesarli619@gmail.com>
2023-02-14 20:42:35 -0500

    Imports required packages

--- designPatterns/src/factory/pattern/pizza_store/styles/ChicagoPizzaStore.java
+++ designPatterns/src/factory/pattern/pizza_store/styles/ChicagoPizzaStore.java
@@ -2,6 +2,10 @@ package factory.pattern.pizza_store.styles;
 
 import factory.pattern.pizza_store.Pizza;
 import factory.pattern.pizza_store.PizzaStore;
+import factory.pattern.pizza_store.pizza_types.ChicagoCheesePizza;
+import factory.pattern.pizza_store.pizza_types.ChicagoClamPizza;
+import factory.pattern.pizza_store.pizza_types.ChicagoPepperoniPizza;
+import factory.pattern.pizza_store.pizza_types.ChicagoVeggiePizza;
 
 public class ChicagoPizzaStore extends PizzaStore{
 	
@@ -9,13 +13,13 @@ public class ChicagoPizzaStore extends PizzaStore{
 	protected Pizza createPizza(String type) {
 		
 		if (type.equals("cheese")) {
-			return new ChicagoStyleCheesePizza();
+			return new ChicagoCheesePizza();
 		} else if (type.equals("veggie")) {
-			return new ChicagoStyleVeggiePizza();
+			return new ChicagoVeggiePizza();
 		} else if (type.equals("pepperoni")) {
-			return new ChicagoStylePepperoniPizza();
+			return new ChicagoPepperoniPizza();
 		} else if (type.equals("clam")) {
-			return new ChicagoStyleClamPizza();			
+			return new ChicagoClamPizza();			
 		} else {
 			return null;
 		}
