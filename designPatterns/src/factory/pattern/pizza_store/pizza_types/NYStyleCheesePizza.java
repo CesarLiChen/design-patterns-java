2757d1f922529e4be9870a26e7701417e0dacfbd
Cesar Li Chen <cesarli619@gmail.com>
2023-02-14 20:10:45 -0500

    Changes modifier on Pizza variables, creates a pizza type

---
+++ designPatterns/src/factory/pattern/pizza_store/pizza_types/NYStyleCheesePizza.java
@@ -0,0 +1,10 @@
+package factory.pattern.pizza_store.pizza_types;
+
+import factory.pattern.pizza_store.Pizza;
+
+public class NYStyleCheesePizza extends Pizza{
+	
+	public NYStyleCheesePizza() {
+		name = "NY Style Sauce and Cheese Pizza";
+	}
+}
