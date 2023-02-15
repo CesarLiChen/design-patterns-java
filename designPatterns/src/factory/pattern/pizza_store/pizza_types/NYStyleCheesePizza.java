34bf4ffda8f82dc3990767b9046bf517b20ccdda
Cesar Li Chen <cesarli619@gmail.com>
2023-02-14 20:13:20 -0500

    Completes NY Cheese pizza

--- designPatterns/src/factory/pattern/pizza_store/pizza_types/NYStyleCheesePizza.java
+++ designPatterns/src/factory/pattern/pizza_store/pizza_types/NYStyleCheesePizza.java
@@ -6,5 +6,9 @@ public class NYStyleCheesePizza extends Pizza{
 	
 	public NYStyleCheesePizza() {
 		name = "NY Style Sauce and Cheese Pizza";
+		dough = "Thin crust dough";
+		sauce = "Marinara Sauce";
+		
+		toppings.add("Grated Reggiano Cheese");
 	}
 }
